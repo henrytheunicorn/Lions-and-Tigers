b2db816fa1f47f1c043de463ab1cfebd300e80aa
Henry Pham <Henry@Vivians-MacBook-Air.local>
2015-04-04 20:54:53 -0700

    four tigers

---
+++ Lions and Tigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  Lions and Tigers
+//
+//  Created by Henry Pham on 4/4/15.
+//  Copyright (c) 2015 Henry Pham. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
